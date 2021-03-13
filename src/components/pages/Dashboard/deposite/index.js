7d9f5828354255c7ce08d9180bfc5db30323eb8c
EzeBernardine <demayous2@gmail.com>
2021-03-13 17:47:03 +0100

    Built desposite page

--- src/components/pages/Dashboard/deposite/index.js
+++ src/components/pages/Dashboard/deposite/index.js
@@ -5,78 +5,134 @@ import {
   Header2,
   Paragraph,
   Header4,
+  Bold,
   Span,
   Header3,
 } from "../../../UI_Components/FontSize/styles";
 import { Link } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
+import { TransferIcon } from "../../../assest/svg";
+import Recepient from "../../../assest/black-beautiful-ladies-smiling 1.png";
 
 const Deposite = () => {
   const validationSchema = yup.object().shape({
     title: yup.string().min(2).required("Provide title"),
-    email: yup.string().min(2).email().required("Provide valid email"),
+    accountnumber: yup.string().min(2).required("Provide valid email"),
   });
   return (
     <Styles className="App">
-      <Flex>
-        
-        
-      <Formik
-            initialValues={{
-              title: "",
-              description: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={async () => {}}
-          >
-            {({}) => (
-              <Form>
-                <Header2 color="#3e3936" lineHeight="5rem">
-                  Register
-                </Header2>
-                <Paragraph
+      <Flex justify="flex-start">
+        <Formik
+          initialValues={{
+            title: "",
+            description: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={async () => {}}
+        >
+          {({}) => (
+            <Form>
+              <Flex margin="0 0 30px 0">
+                <Header4
                   color="#2b180d"
-                  size="14px"
-                  lineHeight="24px"
+                  // size="14px"
+                  // lineHeight="24px"
                   spacing="1.02px"
                 >
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                </Paragraph>
+                </Header4>
+              </Flex>
 
-                <Grid className="input-container" gap="18px">
-                  <div>
-                    <Field type="text" name="name" placeholder="Name" />
-                    <ErrorMessage name="name" component="div" />
-                  </div>
+              <Flex justify="flex-start" margin="30px 0" className="recipient">
+                <Frame height="70px" width="70px" className="recipient-image">
+                  <img src={Recepient} alt="Anonymous" />
+                </Frame>
 
-                  <div>
-                    <Field type="text" name="email" placeholder="Email" />
-                    <ErrorMessage name="email" component="div" />
-                  </div>
+                <Flex
+                  width="max-content"
+                  flexDir="column"
+                  margin="0 0 0 10px"
+                  className="recipient-more-details"
+                  align="flex-start"
+                >
+                  <Span color="#2b180d" size="14px">
+                    You are about to transfer{" "}
+                    <Bold color="#3ede30"> #20000</Bold>
+                  </Span>
+                  <Span color="#2b180d" size="14px">
+                    Recipient: <Bold color="#3ede30">Anonymous Obinna</Bold>
+                  </Span>
+                </Flex>
+              </Flex>
 
-                  <div>
+              <Grid className="input-container" gap="18px">
+                <div>
+                  <Flex className="input-wrap">
+                    <label htmlFor="cardnumber">
+                      How much do you want to deposie
+                    </label>
                     <Field
                       type="text"
-                      name="email"
+                      name="amount"
+                      placeholder="Amount"
+                      id="cardnumber"
+                    />
+                  </Flex>
+                  <ErrorMessage name="amount" component="div" />
+                </div>
+
+                <div>
+                  <Flex className="input-wrap">
+                    <label htmlFor="accountnumber">
+                      Enter account number of recepient
+                    </label>
+                    <Field
+                      type="text"
+                      name="accountnumber"
+                      id="accountnumber"
                       placeholder="Account Number"
                     />
-                    <ErrorMessage name="email" component="div" />
-                  </div>
-                </Grid>
-
-                <div className="btn">
-                  <Link to="./dashboard">
-                    <button type="submit" padding="15px 30px">
-                      Submit
-                    </button>
-                  </Link>
+                  </Flex>
+                  <ErrorMessage name="name" component="div" />
                 </div>
-              </Form>
-            )}
-          </Formik>
 
+                <div>
+                  <Flex className="input-wrap">
+                    <label htmlFor="comment">Comment</label>
+                    <Field
+                      type="text"
+                      name="comment"
+                      id="comment"
+                      component="textarea"
+                      placeholder="Account Number"
+                    />
+                  </Flex>
+                  <ErrorMessage name="comment" component="div" />
+                </div>
 
+                {/* ------------------button section-------------- */}
+                <Flex className="btn" justify="flex-end" margin="23px 0 0 0">
+                  <button type="submit" padding="15px 30px">
+                    <Flex>
+                      <Span
+                        lineHeight="15px"
+                        color={"#fff"}
+                        className="drawerText"
+                      >
+                        Transfer
+                      </Span>
+
+                      <Span lineHeight="15px" color={"#fff"}>
+                        <TransferIcon width="20px" height="20px" />
+                      </Span>
+                    </Flex>
+                  </button>
+                </Flex>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </Flex>
     </Styles>
   );
--- src/components/pages/Dashboard/deposite/styles.js
+++ src/components/pages/Dashboard/deposite/styles.js
@@ -1,5 +1,47 @@
 import styled from "styled-components";
 
 export const Styles = styled.div`
-  
+  form {
+    .recipient {
+      border: 1px solid #3ede305e;
+      background: #56de301f;
+      padding: 30px;
+      .recipient-image {
+        border-radius: 50%;
+      }
+      .recipient-more-details span {
+        font-family: "Regular 400 italic";
+      }
+    }
+
+    .input-container {
+      max-width: 700px;
+      margin-top: 50px;
+      > div {
+        width: 100%;
+        .input-wrap {
+          label {
+            margin-right: 15px;
+          }
+          input {
+            border: 1px solid #e1e1e1;
+            flex: 1;
+            padding: 10px 15px;
+            border-radius: 8px;
+          }
+          textarea {
+            border: 1px solid #e1e1e1;
+            flex: 1;
+            padding: 10px 15px;
+            resize: none;
+            border-radius: 8px;
+          }
+        }
+      }
+    }
+
+    .btn span:first-child {
+      margin-right: 10px;
+    }
+  }
 `;
